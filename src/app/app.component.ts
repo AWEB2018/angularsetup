ad65e5885f292893d9dffac1332c47f38d928be1
santosh <santosh.ece06@gmail.com>
2018-05-15 11:13:04 +0530

    angular setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-dac',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'DACBOOK';
+  postPlaceholder = 'Whats app...';
+
+  postOfDay = '“I can accept failure. Everyone fails at something. But I can’t accept not trying.”';
+  likeCount = 100;
+  dislikeCount = 1000;
+
+  refPost = {
+    'post' : '“I can accept failure. Everyone fails at something. But I can’t accept not trying.”',
+    'likeCount': 100,
+    'dislikeCount': 1000,
+  };
+
+  refPostList = [this.refPost, this.refPost, this.refPost];
 }
